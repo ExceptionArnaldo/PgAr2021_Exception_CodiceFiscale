bbcad056a047754456ef35203b66e7b7289ae63d
yuhang <51709956+yuhang23@users.noreply.github.com>
2021-04-29 17:03:42 +0200

    pulizia main

--- CodiceFiscale/src/it/unibs/fp/codicefiscale/Main.java
+++ CodiceFiscale/src/it/unibs/fp/codicefiscale/Main.java
@@ -14,17 +14,10 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        codiceFiscale c = new codiceFiscale(" ");
-
-
         ArrayList<Persona> persone = new ArrayList<>();
         ArrayList<codiceFiscale> codici_validi = new ArrayList<>();
         ArrayList<codiceFiscale> codici_invalidi = new ArrayList<>();
         ArrayList<codiceFiscale> codici_spaiati = new ArrayList<>();
-
-        //Persona p = new Persona("yuhang", "ye", "M", "soave", "2001-07-23", new codiceFiscale(" "));
-
-        //p.genera_codice_fiscale();
 
         System.out.printf((MSG_LETTURA) + "%n", PERSONE_FILE);
         Xml.leggiPersone(PERSONE_FILE, persone); //1. leggere il file xml di persone e salvare i dati delle persone in un ArrayList
@@ -43,12 +36,6 @@ public class Main {
         Xml.scriviPersone(SCRITTURA_FILE, persone, codici_invalidi, codici_spaiati); //4. scrittura del file xml
 
         Xml.formatXMLFile(SCRITTURA_FILE);
-
-        //codiceFiscale c = new codiceFiscale("GCCSLD65T04D583R");
-        //System.out.println(c.validitàCodice());
-
-        //for (int i = 0; i < persone.size(); i++)
-        //    System.out.println(i + persone.get(i).toString());
 
         System.out.println(persone.size());
         System.out.println(codici_validi.size());
